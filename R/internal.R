1dd4d6c31615a7ae2bab50b10d171e404db2687b
Flo <florent.vouin@gmail.com>
2020-12-16 14:23:44 +0100

    Initial commit with cookiecutter output.

---
+++ R/internal.R
@@ -0,0 +1,15 @@
+.materialdashboard_js_metadata <- function() {
+deps_metadata <- list(`materialdashboard` = structure(list(name = "materialdashboard",
+version = "0.0.1", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = 'materialdashboard.min.js',
+stylesheet = NULL, head = NULL, attachment = NULL, package = "materialdashboard",
+all_files = FALSE), class = "html_dependency"),
+`materialdashboard` = structure(list(name = "materialdashboard",
+version = "0.0.1", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = 'materialdashboard.min.js.map',
+stylesheet = NULL, head = NULL, attachment = NULL, package = "materialdashboard",
+all_files = FALSE, dynamic = TRUE), class = "html_dependency"))
+return(deps_metadata)
+}
