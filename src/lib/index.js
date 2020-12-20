b21d9a0dfd0710fcfffad2b8afba753938e54336
Flo <florent.vouin@gmail.com>
2020-12-20 16:28:24 +0100

    Remove Dashboard component.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,12 +1,10 @@
 /* eslint-disable import/prefer-default-export */
 import TopAppBar from './components/TopAppBar.react';
 import ButtonSelect from './components/ButtonSelect.react';
-import Dashboard from './components/Dashboard.react';
 import MainContentDrawer from './components/MainContentDrawer.react';
 
 export {
     ButtonSelect,
-    Dashboard,
     MainContentDrawer,
     TopAppBar,
 };
