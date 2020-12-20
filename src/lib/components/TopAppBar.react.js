a7f836d0b681f2df93b40fb40d3ecb298f97df64
Flo <florent.vouin@gmail.com>
2020-12-20 16:19:55 +0100

    Add the TopAppBar component.

---
+++ src/lib/components/TopAppBar.react.js
@@ -0,0 +1,86 @@
+import { AppBar, CssBaseline, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1
+    },
+}));
+
+/**
+ * An app bar at the top of the screen.
+ */
+const TopAppBar = (props) => {
+    const classes = useStyles();
+
+    const {
+        children,
+        id,
+        n_menu_clicks,
+        title,
+        setProps
+    } = props;
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+
+            <AppBar className={classes.appBar} id={id} position="fixed">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu" className={classes.menuButton}
+                        onClick={() => setProps({ n_menu_clicks: n_menu_clicks + 1 })}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography className={classes.title} variant="h6">
+                        {title}
+                    </Typography>
+
+                    {children}
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
+
+TopAppBar.defaultProps = {
+    n_menu_clicks: 0
+};
+
+TopAppBar.propTypes = {
+    /**
+     * Content on the right of the app bar.
+     */
+    children: PropTypes.node,
+
+    /**
+     * The ID used to identify this component in Dash callbacks.
+     */
+    id: PropTypes.string.isRequired,
+
+    /**
+     * Clicks made on the menu button.
+     */
+    n_menu_clicks: PropTypes.number,
+
+    /**
+     * A title displayed at the top of the dashboard.
+     */
+    title: PropTypes.string.isRequired,
+
+    /**
+     * Dash-assigned callback that should be called to report property changes
+     * to Dash, to make them available for callbacks.
+     */
+    setProps: PropTypes.func
+};
+
+export default TopAppBar;
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/prefer-default-export */
+import TopAppBar from './components/TopAppBar.react';
 import Dashboard from './components/Dashboard.react';
 
 export {
-    Dashboard
+    Dashboard,
+    TopAppBar,
 };
