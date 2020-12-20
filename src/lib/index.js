d0ee03246ba3c339db6cbdd21510155a96d4dcfe
Flo <florent.vouin@gmail.com>
2020-12-20 16:25:08 +0100

    Add the ButtonSelect component.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable import/prefer-default-export */
 import TopAppBar from './components/TopAppBar.react';
+import ButtonSelect from './components/ButtonSelect.react';
 import Dashboard from './components/Dashboard.react';
 
 export {
+    ButtonSelect,
     Dashboard,
     TopAppBar,
 };
